8048479fa906cdb10fd7035fac85982e7df8f80b
03-junghee <03_junghee@naver.com>
2026-03-27 15:26:03 +0900

    Fix: Add app variable to wsgi for Vercel deployment

--- survibe/wsgi.py
+++ survibe/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for survibe project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'survibe.settings')
 
 application = get_wsgi_application()
 
+# 이 한 줄이 반드시 맨 아래에 있어야 합니다! (오타 주의: 소문자 app)
 app = application
